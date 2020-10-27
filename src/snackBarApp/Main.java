bd84a6eb256b3a00f92a99ca0410e70fdbe508a4
GoldenPedro <pedropablo@casuso.us>
2020-10-26 23:46:53 -0500

    finished Snack class

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,9 @@
+package snackBarApp;
+
+public class Main {
+    public static void main(String[] args) {
+        Snack snack1 = new Snack(1, "Chips", 36, 1.75, 1 );
+        System.out.println(snack1);
+    }
+}
+
